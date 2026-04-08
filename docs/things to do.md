ad86899783dcab4a50b1dcb230a7f4dad74b6b40
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2026-04-08 18:56:38 +0100

    update: refactor workspace, remove duplicate files, step out root by 1, add packages for components, add ignores

---
+++ docs/things to do.md
@@ -0,0 +1,24 @@
+## Key Areas Unassigned
+
+- assumptions and documentation
+- unit tests
+- transaction class 
+- direct debit and standing orders logic.
+- help system 
+- create DB class
+- sequence diagram for setting up a new customer. 
+
+
+### Lucian      
+- [x] ~~make this public~~
+- [ ] improve sequence diagram withdrawal success
+- [ ] create DB
+
+### Callum
+- [ ] start creating menu 
+
+### Harrison
+- [ ] create account classes
+
+### Kim
+ *TBC*
---
+++ src/main/java/org/example/database/CreateDB.java
@@ -0,0 +1,60 @@
+package org.example.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class CreateDB {
+
+    private static final String URL = "jdbc:sqlite:acmebank.db";
+
+    public static Connection connect() throws Exception {
+        return DriverManager.getConnection(URL);
+    }
+
+    public static void initialise() {
+        String createTeller = """
+                CREATE TABLE IF NOT EXISTS Teller (
+                    teller_ID          TEXT PRIMARY KEY,
+                    teller_Name       TEXT NOT NULL,
+                    teller_Password   TEXT NOT NULL
+                );
+                """;
+        String createCustomers = """
+                CREATE TABLE IF NOT EXISTS Customer (
+                    customer_ID          TEXT PRIMARY KEY,
+                    customer_name       TEXT NOT NULL
+                );
+                """;
+        String createAccount = """
+                CREATE TABLE IF NOT EXISTS Account (
+                    account_Number      VARCHAR PRIMARY KEY,
+                    customer_ID     TEXT NOT NULL,
+                    sort_code       TEXT NOT NULL,
+                    balance         TEXT NOT NULL,
+                    account_expiry_date     TEXT NOT NULL,
+                    account_start_date      TEXT NOT NULL,
+                    is_active TEXT NOT NULL
+                );
+                """;
+        String createPersonalAccount= """
+                CREATE TABLE IF NOT EXISTS PersonalACC (
+                    account_Number TEXT NOT NULL,
+                    photo_ID_Verified Boolean
+                )""";
+
+
+        try (Connection conn = connect();
+             Statement stmt = conn.createStatement()) {
+
+            stmt.execute(createCustomers);
+            stmt.execute(createTeller);
+            stmt.execute(createAccount);
+            stmt.execute(createPersonalAccount);
+            System.out.println("Tables created successfully.");
+
+        } catch (Exception e) {
+            System.out.println("Error creating tables: " + e.getMessage());
+        }
+    }
+}
