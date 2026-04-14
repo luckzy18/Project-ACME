305f1259c5761d45555c756c83208aecb3a2abb3
Lucian <lucianmorosan@Lucians-MacBook-Pro.local>
2026-04-14 07:25:41 +0100

    dk why thsi not working

--- ACME/src/main/java/org/example/Database/CreateDB.java
+++ ACME/src/main/java/org/example/Database/CreateDB.java
@@ -2,6 +2,7 @@ package org.example.Database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 
 public class CreateDB {
@@ -25,7 +26,7 @@ public class CreateDB {
                 CREATE TABLE IF NOT EXISTS Customer (
                     customer_ID          TEXT PRIMARY KEY,
                     customer_name       TEXT NOT NULL,
-                    customer_signup_date    TEXT NOT NULL,
+                    customer_signup_date    DATE NOT NULL
                 );
                 """;
         String createAccount = """
@@ -34,41 +35,74 @@ public class CreateDB {
                     customer_ID     TEXT NOT NULL,
                     sort_code       TEXT NOT NULL,
                     balance         TEXT NOT NULL,
-                    account_expiry_date     TEXT NOT NULL,
-                    account_start_date      TEXT NOT NULL,
                     is_active TEXT NOT NULL,
+                    account_type TEXT NOT NULL,
                     FOREIGN KEY (customer_ID) REFERENCES CUSTOMER(customer_id)
                 );
                 """;
         String createPersonalAccount= """
                 CREATE TABLE IF NOT EXISTS PersonalACC (
-                    account_Number TEXT NOT NULL,
+                    account_Number TEXT PRIMARY KEY NOT NULL,
                     photo_ID_Verified Boolean,
                     FOREIGN KEY (account_number) REFERENCES Account(account_number)
                 );""";
         String createISAAccount= """
-                CREATE TABLE IF NOT EXISTS PersonalACC (
-                    account_Number TEXT NOT NULL,
-                    photo_ID_Verified Boolean
+                CREATE TABLE IF NOT EXISTS ISAACC (
+                    account_Number  TEXT PRIMARY KEY NOT NULL,
+                     last_interest_date   TEXT NOT NULL,
+                   FOREIGN KEY (account_number) REFERENCES Account(account_number)
                 );
                 """;
+        String createBusinessAccount= """
+                CREATE TABLE IF NOT EXISTS BUSINESSACC (
+                account_number TEXT PRIMARY KEY,
+                chequeBook BOOLEAN NOT NULL,
+                overdraft_amount INTEGER NOT NULL,
+                overdraft boolean not null,
+                loan_request boolean not null,
+                bussiness_type TEXT NOT NULL,
+                international_trading boolean NOT NULL,
+                FOREIGN KEY (account_number) REFERENCES Account(account_number)
+                 )
+               """;
 
         try (Connection conn = connect();
              Statement stmt = conn.createStatement()) {
 
             stmt.execute(createCustomers);
-            System.out.println("Customers.");
+            System.out.println("Customers created");
             stmt.execute(createTeller);
-            System.out.println("tellers.");
+            System.out.println("tellers created");
             stmt.execute(createAccount);
-            System.out.println("account");
+            System.out.println("account created");
             stmt.execute(createPersonalAccount);
             IO.println("personal account created");
             stmt.execute(createISAAccount);
+            IO.println("ISA account created");
+            stmt.execute(createBusinessAccount);
+            IO.println("business account created");
             System.out.println("Tables created successfully.");
-
+            initialiseMainTeller();
         } catch (Exception e) {
             System.out.println("Error creating tables: " + e.getMessage());
+            e.printStackTrace();
         }
     }
+    private  static void initialiseMainTeller(){
+        String insertMainTeller="INSERT INTO Teller(teller_id,teller_name,teller_Password,teller_role)" +
+                "VALUES(?,?,?,?)";
+        try (Connection conn = connect();
+             PreparedStatement stmt = conn.prepareStatement(insertMainTeller)){
+             stmt.setString(1,"TO01");
+             stmt.setString(2,"ADMIN");
+            stmt.setString(3,"1234");
+            stmt.setString(4,"ADMIN");
+            stmt.executeUpdate();
+            IO.println("Main teller added TO01 Admin");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
 }
