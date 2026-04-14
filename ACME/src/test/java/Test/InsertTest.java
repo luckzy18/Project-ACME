4803e8546238c685a14fcec754d8b4cc77582c69
Lucian <lucianmorosan@Lucians-MacBook-Pro.local>
2026-04-14 14:55:20 +0100

    customer create/ get from db no delete yet

---
+++ ACME/src/test/java/Test/InsertTest.java
@@ -0,0 +1,24 @@
+package Test;
+import org.example.Database.DBinterface;
+import org.example.model.Customer;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class InsertTest {
+
+    @Test
+    void testInsertCustomer() {
+        // Insert the customer
+//        boolean insert=DBinterface.insertCustomer("Test", true, true);
+//        Assert.assertTrue(insert,"failed to insert user");
+        // Retrieve the customer from the DB
+       Customer customer = DBinterface.getCustomerbyID("1");
+
+        // Assert the values are stored correctly
+       Assert.assertNotNull(customer, "Customer should not be null");
+     //   Assert.assertEquals(customer.getName(), "Test", "Customer name should match");
+       // Assert.assertTrue(customer.isActive(), "Customer active flag should be true");
+     //   Assert.assertTrue(customer.isVerified(), "Customer verified flag should be true");
+    }
+
+}
