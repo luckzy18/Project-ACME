c0c178b05d50d9b99c656dbddfc04085e72fb331
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2026-04-20 15:54:21 +0100

    create log type enum + logger class

---
+++ ACME/src/main/java/org/example/logger/Logger.java
@@ -0,0 +1,10 @@
+package org.example.logger;
+
+public class Logger {
+    private LogType type;
+    private String message;
+    private String Source;
+    private int tellerId;
+    private Integer customerId; // use the object instead so it can be null
+    private String accountNumber;
+}
