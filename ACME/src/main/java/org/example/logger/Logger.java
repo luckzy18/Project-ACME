fda35e0fffa602e58e67065d7deb72eb3b868f80
etherchild <81761442+etherOnGitHub@users.noreply.github.com>
2026-04-20 15:59:50 +0100

    add constructor

--- ACME/src/main/java/org/example/logger/Logger.java
+++ ACME/src/main/java/org/example/logger/Logger.java
@@ -1,10 +1,16 @@
 package org.example.logger;
 
 public class Logger {
-    private LogType type;
-    private String message;
-    private String Source;
-    private int tellerId;
-    private Integer customerId; // use the object instead so it can be null
-    private String accountNumber;
+    private final String message;
+    private final String Source;
+    private final int tellerId;
+    private final Integer customerId; // use the object instead so it can be null
+    private final String accountNumber;
+    public Logger(LogType type, String message, String source, int tellerId, Integer customerId, String accountNumber) {
+        this.message = message;
+        this.Source = source;
+        this.tellerId = tellerId;
+        this.customerId = customerId;
+        this.accountNumber = accountNumber;
+    }
 }
